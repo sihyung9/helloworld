5207f661db8ca02e63f86470338b95b6ced2d443
이시형 <sihyeongi649@gmail.com>
2023-03-22 16:39:09 +0900

    1번 추가

---
+++ 0322.py
@@ -0,0 +1,31 @@
+# 2023-03-22
+# 수정
+# ooo
+#ooo
+#
+''' 이것은
+여러줄짜리
+주석입니다.
+'''
+print("오늘은 파이썬 수업입니다. ")
+print("hello !! ")
+print(type("오늘은 파이썬 수업입니다. "))
+
+print(2)
+print(type(2))
+print(5.5)
+
+print("Hello " + " Python")
+print("213 호" * 3)
+print("hello " , " hi " , " 213")
+print("hello ", "hello" , 213, " ", 11111)
+print("hello " + "python")
+print("hello" , 33)
+
+
+print(1,2,-5,3.14,2.71828)
+print('Hi','Python')
+
+print('23000원은','5000원 ?개','1000원 ?개')
+print('5000원',23000 // 5000,'개')
+print('1000원',(23000%5000)//1000,'개')
